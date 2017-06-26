bb62d64d668caa1c7a86963bbcb0b2165b38eb1c
Wagner Coutinho <wagnercoutinho532@gmail.com>
2017-06-26 14:15:43 -0300

    Criação da classe Produto

---
+++ Projeto.Entidades/Entidades/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Entidades.Entidades
+{
+    public class Produto
+    {
+        public int IdProduto { get; set; }
+        public string NomeProduto { get; set; }
+        public string DescricaoProduto { get; set; }
+        public decimal PrecoProduto { get; set; }
+        public string Categoria { get; set; }
+
+    }
+}
