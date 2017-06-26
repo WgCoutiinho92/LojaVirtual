9abc9b97fc4cbfe7d47146bdded58fe87b2d1c41
Wagner Coutinho <wagnercoutinho532@gmail.com>
2017-06-26 16:14:59 -0300

    Instalação e configuração do Entity Framework e criação do banco de dados com a tabela de produtos,

---
+++ Projeto.Dados/Configuracao/ProdutoConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration;
+using Projeto.Entidades.Entidades;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projeto.Dados.Configuracao
+{
+    public class ProdutoConfiguration : EntityTypeConfiguration<Produto>
+    {
+        public ProdutoConfiguration()
+        {
+            ToTable("Produto");
+          {
+                HasKey(p => p.IdProduto);
+
+                Property(p => p.IdProduto)
+               .HasColumnName("IdProduto_PK")
+               .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+                Property(p => p.NomeProduto)
+                    .HasColumnName("Nome_Produto")
+                    .HasMaxLength(150)
+                    .IsRequired();
+
+                Property(p => p.DescricaoProduto)
+                    .HasColumnName("Descricao_Produto")
+                    .HasMaxLength(500)
+                    .IsRequired();
+
+                Property(p => p.PrecoProduto)
+                    .HasColumnName("Preco_Produto")
+                    .HasPrecision(18, 2)
+                    .IsRequired();
+
+            }
+
+        }
+    }
+}
---
+++ Projeto.Dados/DataSource/EfDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Projeto.Entidades.Entidades;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Configuration;
+using Projeto.Dados.Configuracao;
+
+namespace Projeto.Dados.DataSource
+{
+    public class EfDbContext : DbContext
+    {
+        public EfDbContext() : base(ConfigurationManager.ConnectionStrings["EfDbContext"].ConnectionString)
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new ProdutoConfiguration());
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produto");
+        }
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
