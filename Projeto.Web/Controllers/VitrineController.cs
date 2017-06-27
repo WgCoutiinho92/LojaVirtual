92e6e2f83f0486c1f06a37dcdba9a4b8167f64ec
Wagner Coutinho <wagnercoutinho532@gmail.com>
2017-06-27 16:06:19 -0300

    Criação de classes para uso de paginação na view Vitrine

---
+++ Projeto.Web/Controllers/VitrineController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projeto.Dados.DataSource;
+
+namespace Projeto.Web.Controllers
+{
+    public class VitrineController : Controller
+    {
+        private EfDbContext pr;
+        public int ProdutoPorPagina = 2;
+        // GET: Vitrine
+        public ActionResult ListaProdutos(int pagina = 1)
+        {
+            pr = new EfDbContext();
+            var produtos = pr.Produto.OrderBy(p => p.DescricaoProduto)
+                .Skip((pagina - 1) * ProdutoPorPagina)
+                .Take(ProdutoPorPagina);
+
+            return View(produtos);
+        }
+    }
+}
